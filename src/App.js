b0622c8a9fc88f61440ea4d14b4dd4cb6c8eb408
Assassin323 <156573789+Assassin323@users.noreply.github.com>
2025-09-16 21:50:13 +0530

    changed HashRouter in app.js for routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import FormPage from './FormPage';
 import DetailsPage from './DetailsPage';
 
